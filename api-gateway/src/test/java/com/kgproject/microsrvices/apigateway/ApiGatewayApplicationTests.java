597db387ab4cc489a01e394f619322e8f5ebc1f3
Gupta <khygupta@deloitte.com>
2021-09-10 13:42:21 +0530

    api gateway added

---
+++ api-gateway/src/test/java/com/kgproject/microsrvices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgproject.microsrvices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
